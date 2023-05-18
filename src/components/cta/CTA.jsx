ab2be521b759eaf4e26e5e4337fdc64ebcb12d38
ri-commits <kartashourychard@gmail.com>
2023-05-18 20:29:01 +0300

    CTA JSX

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,17 @@
 import "./cta.css";
 
 const Cta = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gtp3__cta">
+      <div className="gtp3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
