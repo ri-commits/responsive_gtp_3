4cabdf4ab95e6d6b13a6d6c5f252119da8e13a58
ri-commits <kartashourychard@gmail.com>
2023-05-18 11:07:09 +0300

    Header Styles

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div className="gpt3_header section__padding" id="home">
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
@@ -23,9 +23,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
