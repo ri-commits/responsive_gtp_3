a7ff4bd307da741297b613493c59e6d119510857
ri-commits <kartashourychard@gmail.com>
2023-05-18 20:45:46 +0300

    CTA Styles

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -7,7 +7,7 @@ const Cta = () => {
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
       </div>
-      <div className="gpt3__cta-btn">
+      <div className="gtp3__cta-btn">
         <button type="button">Get Started</button>
       </div>
     </div>
