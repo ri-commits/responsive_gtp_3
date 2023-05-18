5026121713c22be78a1e70b7fb9a7fe17a310799
ri-commits <kartashourychard@gmail.com>
2023-05-18 16:48:01 +0300

    whatGPT Styles

--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -1,7 +1,25 @@
+import { Feature } from "../../components";
 import "./whatGPT.css";
 
 const WhatGPT = () => {
-  return <div>WhatGPT</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT;
