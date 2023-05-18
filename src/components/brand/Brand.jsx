3daa8810dc022d63056be7a46e23ae38649d4960
ri-commits <kartashourychard@gmail.com>
2023-05-18 16:19:22 +0300

    Brand JSX

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,26 @@
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports.js";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slag" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
